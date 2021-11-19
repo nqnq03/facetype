90b5a63f669e9592476ba47da6cb459170fc27f4
nqnq03 <65667126+nqnq03@users.noreply.github.com>
2021-11-19 16:02:11 +0900

    Update facetype2.py

--- facetype2.py
+++ facetype2.py
@@ -112,7 +112,7 @@ def upload_file():
         return render_template("index.html",answer="")
 
 
+
 if __name__ == "__main__":
     app.debug = True
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host ='0.0.0.0',port = port)
+    app.run()
